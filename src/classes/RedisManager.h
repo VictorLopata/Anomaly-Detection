006a781c716d819a7b3b71bb452d048fc115e7b4
VictorLopata <victorlopata2002@gmail.com>
2024-08-17 16:18:39 +0200

    Redis Manager

---
+++ src/classes/RedisManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Victor Lopata on 17/08/24.
+//
+
+#ifndef ANOMALY_DETECTION_REDISMANAGER_H
+#define ANOMALY_DETECTION_REDISMANAGER_H
+
+
+
+class RedisManager {
+
+};
+
+
+
+#endif //ANOMALY_DETECTION_REDISMANAGER_H

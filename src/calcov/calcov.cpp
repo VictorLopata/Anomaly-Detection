64a2a973ce5ac321362abf0592cf1bb72dcdeaab
A J <jiang.1989177@studenti.uniroma1.it>
2024-10-01 18:22:23 +0200

    prova

---
+++ src/calcov/calcov.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class CovarianceCalculator{
+
+    public :
+        CovarianceCalculator(std::vector<double>s1,std::vector<double> s2){
+        std::vector<double> st1={s1};
+        std::vector<double> st2={s2};
+        }
+    
+    float cal_cov(float s1,float s2){
+        for (size_t i = 0; i < sizeof(s1); i++)
+        {
+            
+            /* code */
+        }
+        
+    } 
+};
+
+int main(){
+
+
+
+    
+}
+
+

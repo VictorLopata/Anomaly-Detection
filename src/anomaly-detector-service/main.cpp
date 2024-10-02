b24d8818b39558f0e627725f1eed204947d0dd5a
VictorLopata <victorlopata2002@gmail.com>
2024-10-02 16:14:36 +0200

    Ascolto streamzzz

--- src/anomaly-detector-service/main.cpp
+++ src/anomaly-detector-service/main.cpp
@@ -2,105 +2,102 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <map>
 #include "main.h"
 #include "../con2redis/con2redis.h"
 
 
 using namespace std;
 
+
+int binomialCoeff(int n, int k) {
+    int ans = 1;
+    if (k > n - k) {
+        k = n - k;
+    }
+    for (int i = 0; i < k; i++) {
+        ans = ans * (n - i);
+        ans = ans / (i + 1);
+    }
+    return ans;
+}
+
 int main() {
     // Connessione a Redis
     redisContext *c = redisConnect(REDIS_SERVER, REDIS_PORT);
-    if (c == NULL || c->err) {
-        if (c) {
-            std::cerr << "Errore: " << c->errstr << std::endl;
-        } else {
-            std::cerr << "Non posso allocare il contesto Redis" << std::endl;
-        }
-        exit(1);
-    }
+
 
     // Aspetta la configurazione sulla stream conf
     config conf = getConf(c);
-
     cout << "n_stream: " << conf.num_streams << " Threshold: "<< conf.threshold << " W: "<< conf.W <<endl;
-    /**
-    vector<string> streams = {"stream1", "stream2", "stream3"};
-    vector<string> stream_ids(streams.size(), "$");
+    int numStreamCov = binomialCoeff(conf.num_streams, 2);
+    int numTotStream = numStreamCov + conf.num_streams;
 
-    while (true) {
-        // Costruisci il comando XREAD
-        string command = "XREAD BLOCK 0 STREAMS";
-        for (const auto& stream : streams) {
-            command += " " + stream;
+    auto* streams = new string[numTotStream];
+    auto* avgCurr = new double[conf.num_streams];
+    auto* covCurr = new double[numStreamCov];
+
+    // Inserimento nomi stream medie e delle covarianze
+    for (int i = 0; i < numTotStream; i++){
+        string strName;
+        if (i < conf.num_streams) {
+            strName = "avgS" + to_string(i);
+        } else {
+            strName = "covN" + to_string(i);
         }
-        for (const auto& id : stream_ids) {
-            command += " " + id;
+        streams[i] = strName;
+    }
+
+    // Ascolto le stream su Redis.
+
+    // Mappa per tenere traccia degli ultimi ID letti per ogni stream
+    map<string, string> lastID;
+    for (int i = 0; i < numTotStream; i++)
+    {
+        lastID[streams[i]] = "$";
+    }
+
+    while (1) {
+        // Composizione del comando XREAD
+        string com = "XREAD COUNT 1 BLOCK 0 STREAMS";
+        for (int i = 0; i < numTotStream; i++) {
+            com += " " + streams[i];
         }
 
-        // Invia il comando
-        redisReply *reply = (redisReply *)redisCommand(c, command.c_str());
-        if (reply == NULL) {
-            std::cerr << "Errore: " << c->errstr << std::endl;
-            redisFree(c);
-            exit(1);
+        for (int i = 0; i < numTotStream; i++) {
+            com += " " + lastID[streams[i]];
         }
-
-        if (reply->type == REDIS_REPLY_NIL) {
-            // Nessun nuovo messaggio
-            freeReplyObject(reply);
-            continue;
-        }
-
-        if (reply->type != REDIS_REPLY_ARRAY) {
-            std::cerr << "Tipo di risposta inaspettato: " << reply->type << std::endl;
-            freeReplyObject(reply);
-            continue;
-        }
-
-
-        for (size_t i = 0; i < reply->elements; ++i) {
-            redisReply *stream_reply = reply->element[i];
-            if (stream_reply->type != REDIS_REPLY_ARRAY || stream_reply->elements != 2) {
-                continue;
-            }
-
-            std::string stream_name = stream_reply->element[0]->str;
-            redisReply *messages = stream_reply->element[1];
-
-            for (size_t j = 0; j < messages->elements; ++j) {
-                redisReply *message = messages->element[j];
-                if (message->type != REDIS_REPLY_ARRAY || message->elements != 2) {
-                    continue;
-                }
-
-                std::string message_id = message->element[0]->str;
-                redisReply *fields = message->element[1];
-
-                std::cout << "Stream: " << stream_name << ", ID: " << message_id << std::endl;
-                for (size_t k = 0; k < fields->elements; k += 2) {
-                    std::string field_name = fields->element[k]->str;
-                    std::string field_value = fields->element[k + 1]->str;
-                    std::cout << field_name << ": " << field_value << std::endl;
-                }
-                std::cout << "------------------------" << std::endl;
-
-                // Aggiorna l'ID dello stream
-                for (size_t s = 0; s < streams.size(); ++s) {
-                    if (streams[s] == stream_name) {
-                        stream_ids[s] = message_id;
-                        break;
+        // Esecuzione comando
+        redisReply* reply = (redisReply*)redisCommand(c, com.c_str());
+        if (reply == nullptr) {
+            cerr << "Errore nell'esecuzione del comando XREAD" << endl;
+            break;
+        } else {
+            for (int i = 0; i < reply->elements; ++i) {
+                redisReply* streamReply = reply->element[i];
+                string nomeStream = streamReply->element[0]->str;
+                redisReply* entriesReply = streamReply->element[1];
+                for (int j = 0; j < entriesReply->elements; ++j) {
+                    redisReply* entryReply = entriesReply->element[j];
+                    string entryID = entryReply->element[0]->str;
+                    redisReply* fieldsReply = entryReply->element[1];
+                    cout << "Stream: " << nomeStream << ", ID: " << entryID << std::endl;
+                    for (size_t k = 0; k < fieldsReply->elements; k += 2)
+                    {
+                        // TODO: Confronta con media corrente, se supera threshold, allora anomalia. Salva nel database.
+                        string campo = fieldsReply->element[k]->str;
+                        string valore = fieldsReply->element[k+1]->str;
+                        cout << "  " << campo << ": " << valore << endl;
                     }
+                    lastID[nomeStream] = entryID;
                 }
             }
         }
-
         freeReplyObject(reply);
     }
 
 
 
-     **/
     redisFree(c);
     return 0;
 }

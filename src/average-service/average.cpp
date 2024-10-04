09618b909fe89a7df61e68745b129e3e5b806385
dieguz <guzmanaguirre.2006301@studenti.uniroma1.it>
2024-10-04 11:30:44 +0200

    continue avg class (TO DO: how i send the value to confront)

--- src/average-service/average.cpp
+++ src/average-service/average.cpp
@@ -1,11 +1,18 @@
 #include "average.h"
 #include <iostream>
 
-Average::Average(const string& redis_host, const string& redis_port) {
+Average::Average(const string& redis_host, const string& redis_port, int n_sensors, int windowSize) {
   c = redisConnect(redis_host.c_str(), redis_port);
-  if (c == NULL || c->err) {
+  if (c == nullptr || c->err) {
     cerr << "Error connecting to redis at " << redis_host << ":" << redis_port << endl;
     exit(1);
+  }
+  this->n_sensors = n_sensors;
+  this->windowSize = windowSize;
+
+  for (int i = 0; i < n_sensors; i++) {
+    string streamName = "stream_" + to_string(i+1);
+    values[streamName] = vector<float>();
   }
 }
 
@@ -15,8 +22,18 @@ Average::~Average() {
   }
 }
 
+void Average::getValueStream(const string& streamName, float value) {
+  values[streamName].push_back(value);
+}
+
+double Average::calculate_average(const vector<double> &values) {
+
+}
+
+
 void Average::listenStreams(const vector<string>& streams) {
   for (const string& stream : streams) {
-
+    // Read the values from the N streams of redis
+    redisReply* reply = (redisReply*)redisCommand(c, "XREAD COUNT 1 STREAMS %s 0", count, stream);
   }
 }
--- src/average-service/average.h
+++ src/average-service/average.h
@@ -3,25 +3,32 @@
 
 #include <iostream>
 #include "../con2redis/con2redis.h"
+#include <map>
 
 using namespace std;
 
 class Average {
     public:
         // Constructor for connecting to Redis
-        Average(const string& redis_host, const string& redis_port);
+        Average(const string& redis_host, const string& redis_port, int n_sensors, int windowSize);
 
         // Destructor for cleaning the connection
         ~Average();
 
+        // Listen to the streams Redis
         void listenStreams(const vector<string>& streams);
+
+        // Store the value from a stream (sensor) in the array
+        void getValueStream(const string& stream, float value);
 
         // This method calculates average from the streams (Sensors in our project)
         double calculate_average(const vector<double>& values);
 
     private:
         RedisContext* c; // Connection to Redis
-        vector<double> values; //This contains the values i store from a window W
+        int n_sensors; // Number of streams (sensors)
+        int windowSize; // Time W after which we compute the average
+        map<string, vector<float>> values; // This contains values from each sensor in a time W
 };
 
 
